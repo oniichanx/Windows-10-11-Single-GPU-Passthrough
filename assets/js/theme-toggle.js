c5b9813de57802c579eb6b5c7c66aac57231f8f4
oniichanx <aom1012@hotmail.com>
2024-08-04 09:14:38 +0000

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleButton = document.getElementById("theme-toggle");
+    const lightThemeLink = document.getElementById("light-theme");
+    const darkThemeLink = document.getElementById("dark-theme");
     const currentTheme = localStorage.getItem("theme") || "light";
-    
-    document.body.classList.add(currentTheme + "-theme");
+
+    if (currentTheme === "dark") {
+        darkThemeLink.removeAttribute("disabled");
+        lightThemeLink.setAttribute("disabled", "disabled");
+    } else {
+        lightThemeLink.removeAttribute("disabled");
+        darkThemeLink.setAttribute("disabled", "disabled");
+    }
 
     themeToggleButton.addEventListener("click", () => {
-        let newTheme = document.body.classList.contains("light-theme") ? "dark" : "light";
-        document.body.classList.remove("light-theme", "dark-theme");
-        document.body.classList.add(newTheme + "-theme");
-        localStorage.setItem("theme", newTheme);
+        if (document.body.classList.contains("light-theme")) {
+            document.body.classList.remove("light-theme");
+            document.body.classList.add("dark-theme");
+            darkThemeLink.removeAttribute("disabled");
+            lightThemeLink.setAttribute("disabled", "disabled");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.body.classList.remove("dark-theme");
+            document.body.classList.add("light-theme");
+            darkThemeLink.setAttribute("disabled", "disabled");
+            lightThemeLink.removeAttribute("disabled");
+            localStorage.setItem("theme", "light");
+        }
     });
 });
