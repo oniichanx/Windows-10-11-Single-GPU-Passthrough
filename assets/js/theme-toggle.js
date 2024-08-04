a0ab8ae5b9306a7b4781c5b2e47dd23afc15ccee
oniichanx <aom1012@hotmail.com>
2024-08-04 09:14:21 +0000

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const themeToggleButton = document.getElementById("theme-toggle");
-  const currentTheme = localStorage.getItem("theme") || "light";
-  
-  document.body.classList.add(currentTheme + "-theme");
-  
-  themeToggleButton.addEventListener("click", () => {
-    let newTheme = document.body.classList.contains("light-theme") ? "dark" : "light";
-    document.body.classList.remove("light-theme", "dark-theme");
-    document.body.classList.add(newTheme + "-theme");
-    localStorage.setItem("theme", newTheme);
-  });
+    const themeToggleButton = document.getElementById("theme-toggle");
+    const currentTheme = localStorage.getItem("theme") || "light";
+    
+    document.body.classList.add(currentTheme + "-theme");
+
+    themeToggleButton.addEventListener("click", () => {
+        let newTheme = document.body.classList.contains("light-theme") ? "dark" : "light";
+        document.body.classList.remove("light-theme", "dark-theme");
+        document.body.classList.add(newTheme + "-theme");
+        localStorage.setItem("theme", newTheme);
+    });
 });
